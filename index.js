2574fb7605b8d8e39e9ca85abb7dc72ad99652c0
joeneedum <joeneedum@gmail.com>
2022-05-23 20:47:06 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const PORT = 8000
 const axios = require("axios");
 const express = require("express")
+const cors = require("cors")
 require('dotenv').config()
 const app = express()
+
+app.use(cors())
 
 app.listen(PORT, () => console.log('Server running on port' + PORT))
 
@@ -10,7 +13,7 @@ app.get('/word', (req, res) => {
     const options = {
         method: 'GET',
         url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-        params: {count: '5'},
+        params: {count: '5', wordLength: '5'},
         headers: {
             'X-RapidAPI-Host': 'random-words5.p.rapidapi.com',
             'X-RapidAPI-Key': process.env.RAPID_API_KEY
